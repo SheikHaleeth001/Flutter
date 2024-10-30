c8b81baa717e73f0a32e6d8f50b46623b9365858
Sheik <55941177+SheikHaleeth001@users.noreply.github.com>
2024-10-30 08:18:23 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,21 @@
+module ZenitusHTML
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/felixge/httpsnoop v1.0.2
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/cors v1.2.1
+	github.com/golang-migrate/migrate/v4 v4.15.1
+	github.com/google/uuid v1.3.0
+	github.com/jackc/pgconn v1.8.0
+	github.com/jackc/pgx/v4 v4.10.1
+	github.com/jasonlvhit/gocron v0.0.1
+	github.com/lib/pq v1.10.3
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.9.0
+	go.uber.org/zap v1.17.0
+
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+//go:generate pipenv run python ./scripts/generate/generate.py
+
+import (
+	"ZenitusHTML/configuration"
+	"ZenitusHTML/constants"
+	"ZenitusHTML/router"
+	"ZenitusHTML/utils"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const MaxHeaderBytes int = 1 << 20
+
+func main() {
+	fmt.Println("aa")
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	configPath := flag.String("conf", "", "configuration folder path give without trailing slash")
+	flag.Parse()
+	fmt.Println("a")
+	if flag.NFlag() != 1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fmt.Println("b")
+
+	filePath := strings.TrimSpace(*configPath) + "/config.json"
+	schemaPath := strings.TrimSpace(*configPath) + "/schema.json"
+
+	// config init
+	cfg := configuration.NewViperConfig()
+	config, err := cfg.Init(filePath, schemaPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Determine port for HTTP service.
+	port := config.GetString(utils.APP_PORT)
+	if port == "" {
+		port = "9090"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	mux, closeMux := router.NewChiRouter(cfg).InitRouter()
+
+	// Start HTTP server.
+	server := &http.Server{
+		Addr:           ":" + port,
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.GetInt(constants.SrvReadTimeOut)) * time.Second,
+		WriteTimeout:   time.Duration(config.GetInt(constants.SrvWriteTimeout)) * time.Second,
+		IdleTimeout:    time.Duration(config.GetInt(constants.SrvMaxIdleTimeOut)) * time.Second,
+		MaxHeaderBytes: MaxHeaderBytes,
+	}
+
+	server.RegisterOnShutdown(closeMux)
+	server.RegisterOnShutdown(config.Close)
+
+	// Graceful shut down of server
+	graceful := make(chan os.Signal, 1)
+	signal.Notify(graceful, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-graceful
+		log.Println("Shutting down server...")
+
+		timeout := time.Duration(config.GetInt(constants.SrvTimeOut)) * time.Second
+		ctx, cancelFunc := context.WithTimeout(context.Background(), timeout)
+		defer cancelFunc()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatalf("Could not do graceful shutdown: %v\n", err)
+		}
+	}()
+
+	log.Println("Listening server on ", port)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("Not able to start server on port: %v error: %s", config.GetString(constants.Port), err)
+	}
+
+	log.Println("Server gracefully stopped...")
+}
